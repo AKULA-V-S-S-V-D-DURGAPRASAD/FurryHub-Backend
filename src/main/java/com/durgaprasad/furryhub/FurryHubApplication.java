8d18d9bbf0fd5564af08be6929f58d4e3e39f143
FurryHub Backend <akuladurgaprasad5@gmail.com>
2025-08-26 22:49:25 +0530

    Project Update

---
+++ src/main/java/com/durgaprasad/furryhub/FurryHubApplication.java
@@ -0,0 +1,13 @@
+package com.durgaprasad.furryhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurryHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/durgaprasad/furryhub/FurryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgaprasad.furryhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurryHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
